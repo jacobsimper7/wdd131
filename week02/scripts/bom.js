4de92df3941bcc140f76405fe76c22531019a400
jacobsimper7 <jacobsimper777@gmail.com>
2025-05-14 22:34:50 -0700

    Fixed erros

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,23 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = '❌'
-li.append(deleteButton);
-list.append(li);
+const button = document.querySelector('button');
+const input = document.querySelector('#favchap');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌'
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+        });
+    }
+});
